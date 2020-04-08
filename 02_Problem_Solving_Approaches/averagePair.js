39c6f09408e5826f11ddec120d9981f0fab40e33
Molly Seeley <mseeley89@gmail.com>
2020-04-08 11:43:56 -0400

    averagPair prompt

---
+++ 02_Problem_Solving_Approaches/averagePair.js
@@ -0,0 +1,12 @@
+/*
+AVERAGE PAIR
+Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair in the array whose average is equal to the target average. There may be more than one pair in the target.
+
+Function MUST be O (n)/O(1) time/space
+
+Test Cases:
+averagePair([1, 2, 3], 2.5) // true
+averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8) //true
+averagePair([-1, 0, 3, 4, 4, 5, 6], 4.1) // false
+averagePair([], 2) // false
+*/
