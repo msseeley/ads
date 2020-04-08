f0bdae1b77a3dbab1fbe9734cf7f2a5ac6a7e59a
Molly Seeley <mseeley89@gmail.com>
2020-04-08 12:11:04 -0400

    averagePair approach and prompt

--- 02_Problem_Solving_Approaches/averagePair.js
+++ 02_Problem_Solving_Approaches/averagePair.js
@@ -9,4 +9,14 @@ averagePair([1, 2, 3], 2.5) // true
 averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8) //true
 averagePair([-1, 0, 3, 4, 4, 5, 6], 4.1) // false
 averagePair([], 2) // false
+
+APPROACH
+- establish pointers at the start and end of the array
+- take the avg of the values at the indices of the pointers
+- based on the average
+  - return true if average is equal to the target average
+  - move start pointer to the right if the average is less than the target average
+  - move end pointer to the left if the average is greater than the target average
 */
+
+
