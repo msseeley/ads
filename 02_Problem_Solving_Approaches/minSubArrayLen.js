b29f85033792d329eb220336e0c580e94131ae6e
Molly Seeley <mseeley89@gmail.com>
2020-04-08 14:40:45 -0400

    minSubArrayLen prompt

---
+++ 02_Problem_Solving_Approaches/minSubArrayLen.js
@@ -0,0 +1,11 @@
+/*
+MINIMUM SUBARRAY LENGTH
+Write a function called minSubArrayLen, which accepts two parameters - an array of positive integers and a number 'n'. This function should return the length of the smallest length of consecutive subarray with a sum greater than or equal to n. If there isn't one return 0 instead.
+Complexity MUST be O(n)/O(1) Time/Space.
+
+
+Test Cases:
+  minSubArrayLen([2, 3, 1, 2, 4, 3], 4) // 2
+  minSubArrayLen([2, 1, 6, 5, 4], 9) //2
+  minSubArrayLen([1, 2, 3], 35) //0
+*/
