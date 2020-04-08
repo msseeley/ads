d343163d11c9c5d6ca26c76811f73d7f6558f7e9
Molly Seeley <mseeley89@gmail.com>
2020-04-08 10:47:10 -0400

    notes apporoach sameFrequencies

--- 02_Problem_Solving_Approaches/problem_sets.js
+++ 02_Problem_Solving_Approaches/problem_sets.js
@@ -9,7 +9,19 @@ sameFrequency(34, 14) //true
 sameFrequency(3589578, 5879385) // true
 sameFrequency(22,222)// false
 */
-
+/*
+APPROACH:
+ - translating the numbers into something iterable -> string
+ - small optimization if the two strings aren't equal they can't have same frequency of digits
+ - create variable to store frequencies
+ - go through one string and store frequencies
+ - go through the other string and decrement from frequencies
+    - if value isn't in frequencies the strings or the value is 0 (falsey) return false
+      - the strings don't have the same num of frequencies
+    - if the valuse is in frequencies decrement it
+ - if going through the second string doesn't return a false, strings must have same frequencies
+ - return true
+*/
 function sameFrequency(numA, numB) {
   const strA = numA.toString();
   const strB = numB.toString();
