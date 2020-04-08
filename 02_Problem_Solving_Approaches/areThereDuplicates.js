ea0a1fdce611a334df8377a39c42559e67396767
Molly Seeley <mseeley89@gmail.com>
2020-04-08 11:28:43 -0400

    approach and solution areThereDuplicates O(n logn)/O(1) time/space

--- 02_Problem_Solving_Approaches/areThereDuplicates.js
+++ 02_Problem_Solving_Approaches/areThereDuplicates.js
@@ -12,13 +12,31 @@ Test Cases:
   areThereDuplicates('a', 'b', 'c') // false
 
 APPROACH:
+- optimization: if ...args.length < 2 return false
 Space: O(n) Time: O(n)
   - create a set to store arguments
   - compare set size to arguments length
     - if they are not equal, then it's TRUE they have duplicates
     - if they are equal,, then it's FALSE that they have duplicates
+
+Space: O(1) Time: O (n logn)
+  - sort arguments
+  - loop through arguments to see if the values are ever equal to neighbor
+    - if values are equal to a neighbor - return true
+    - if values are never equal - return false
 */
 
 function areThereDuplicates(...args) {
   return Set(args).size !== args.length;
+}
+
+function areTherDuplicatesV2(...args) {
+  if (args.length < 2) return false;
+  args.sort();
+  for (let i = 1; i < args.length; i++) {
+    const neighbor = args[i - 1];
+    const current = args[i];
+    if (neighbor === current) return true;
+  }
+  return false;
 }
