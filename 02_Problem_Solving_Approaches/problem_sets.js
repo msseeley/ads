1ce45cc8eb858f3ba1719ed045ec6fdd929095f7
Molly Seeley <mseeley89@gmail.com>
2020-04-08 10:19:44 -0400

    package json

---
+++ 02_Problem_Solving_Approaches/problem_sets.js
@@ -0,0 +1,12 @@
+/*
+FREQUENCY COUNTER
+Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits.
+The function MUST have an Time Complexity of O (n).
+
+Test Cases:
+sameFrequency(182, 281) // true
+sameFrequency(34, 14) //true
+sameFrequency(3589578, 5879385) // true
+sameFrequency(22,222)// false
+*/
+
