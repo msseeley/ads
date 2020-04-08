753c3a33cb8e61fb7d279ac171de894f7a8411be
Molly Seeley <mseeley89@gmail.com>
2020-04-08 13:27:01 -0400

    isSubsequence prompt

---
+++ 02_Problem_Solving_Approaches/isSubsequence.js
@@ -0,0 +1,13 @@
+/*
+IS SUBSEQUENCE
+Write a function called isSubsequence, that takes in two strings and returns whether the first string is a substring of the second string. Specifically, the function should check if the characters in the first are present and in the same order of appearance as the second string.
+
+The function MUST be at least  O(n+m)/O(1) time/space.
+
+Test cases
+  isSubsequence('hello', 'hello world') //true
+  isSubsequence('sing', 'sting') //true
+  isSubsequence('abc', 'abracadabra') //true
+  isSubsequence('abc', 'cba') // false
+
+*/
