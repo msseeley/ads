f857a373a6e60ae20afb0d05d302fb1be3c2eb59
Molly Seeley <mseeley89@gmail.com>
2020-04-10 15:28:02 -0400

    isPalindrome approaches

--- 03_Recursion/isPalindrome.js
+++ 03_Recursion/isPalindrome.js
@@ -8,6 +8,19 @@ isPalindrome('tacocat') //true
 isPalindrome('amanaplanacanalpanama') //true
 
 APPROACH:
+  recursive only
+   - Base Cases:
+      if there's only one ltr it's the same reversed
+      if there's two ltrs compare see if they're equal
+   - Recursive Case:
+      compare the ends of the string
+      if they're the same ltr continue with recursive call
+      input decrement by taking the beginning and end characters off
+      if not return false
+
+  recursive helper
+    - use reverse string recursion (see reverse.js)
+    - compare initial string with reversed string
 
 */
 
