835890d691ba958174da9bda7595fde55026a69f
Molly Seeley <mseeley89@gmail.com>
2020-06-26 16:28:56 -0400

    insert test and method

--- 09_Trees/Binary_Search_Tree.js
+++ 09_Trees/Binary_Search_Tree.js
@@ -11,10 +11,21 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insert(value, curr = this.root) {
-    // if (!curr) curr = new Node(value);
-    // let dir = curr.value > value ? 'left' : 'right';
-    // curr = this.insert(value, curr[dir]);
+  insert(value) {
+    const node = new Node(value);
+    if (!this.root) {
+      this.root = node;
+    } else {
+      let curr = this.root;
+      let dir;
+      while (curr) {
+        dir = curr.value > value ? "left" : "right";
+        if (curr[dir]) curr = curr[dir];
+        else break;
+      }
+      curr[dir] = new Node(value)
+    }
+    return this;
   }
 
   search() {
--- test/test_Binary_Search_Tree.js
+++ test/test_Binary_Search_Tree.js
@@ -24,6 +24,34 @@ describe("Binary Search Tree", () => {
       expect(node).to.have.property('left');
       expect(node).to.have.property('right');
     });
-
-  })
+  });
+  describe("insert method", () => {
+    const bst = new BinarySearchTree();
+    it("inserts the first node at the root of the tree", () => {
+      bst.insert(50);
+      expect(bst.root.value).to.equal(50);
+      expect(bst.root.left).to.equal(null);
+      expect(bst.root.right).to.equal(null);
+    });
+    it("inserts subsequent nodes in the appropriate order", () => {
+      bst.insert(45);
+      bst.insert(59);
+      bst.insert(20);
+      bst.insert(5);
+      bst.insert(47);
+      bst.insert(78);
+      bst.insert(90);
+      bst.insert(6);
+      bst.insert(88);
+      expect(bst.root.left.value).to.equal(45);
+      expect(bst.root.left.right.value).to.equal(47);
+      expect(bst.root.left.left.value).to.equal(20);
+      expect(bst.root.left.left.left.value).to.equal(5);
+      expect(bst.root.left.left.left.right.value).to.equal(6);
+      expect(bst.root.right.value).to.equal(59);
+      expect(bst.root.right.right.value).to.equal(78);
+      expect(bst.root.right.right.right.value).to.equal(90);
+      expect(bst.root.right.right.right.left.value).to.equal(88);
+    });
+  });
 })
