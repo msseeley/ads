92ab913ec0b0c16af575ef276cca0c6aa2d90755
Molly Seeley <mseeley89@gmail.com>
2020-07-13 15:51:48 -0400

    bug fix test

--- 09_Trees/Max_Heap.js
+++ 09_Trees/Max_Heap.js
@@ -9,6 +9,27 @@ class MaxHeap {
     this.values = [];
   }
   //helper functions
+  findParentIdx(idx) {
+    return Math.floor((idx - 1) / 2);
+  }
+
+  swap(childIdx, parentIdx) {
+    let child = this.values[childIdx];
+    this.values[childIdx] = this.values[parentIdx];
+    this.values[parentIdx] = child;
+  }
+
+  insert(value) {
+    this.values.push(value);
+    let idx = this.values.length - 1;
+    let parentIdx = Math.floor((idx - 1) / 2);
+    while (this.values[parentIdx] < value) {
+      this.swap(idx, parentIdx);
+      idx = parentIdx;
+      parentIdx = Math.floor((idx - 1) / 2);
+    }
+    return this.values;
+  }
 }
 
 module.exports = MaxHeap;
--- test/test_Max_Heap.js
+++ test/test_Max_Heap.js
@@ -5,13 +5,13 @@ describe.only("Max Heap", () => {
   it("inserts values so that parents are greater than children", () => {
     const heap = new MaxHeap();
     [12, 14, 2, 50, 8, 10, 36, 42].forEach(val => heap.insert(val));
-    expect(heap.values).to.eql([50, 42, 36, 14, 8, 10, 12, 2]);
+    expect(heap.values).to.eql([50, 42, 36, 14, 8, 2, 10, 12]);
     expect(heap.values[1]).to.be.above(heap.values[3]);
     expect(heap.values[1]).to.be.above(heap.values[4]);
     expect(heap.values[0]).to.be.above(heap.values[1]);
     expect(heap.values[0]).to.be.above(heap.values[2]);
     expect(heap.values[3]).to.be.above(heap.values[7]);
-    expect(heap.values[3]).to.be.above(heap.values[8]);
+    expect(heap.values[3]).to.be.above(heap.values[7]);
   });
   it("inserts values filling the left side first", () => {
     const heap = new MaxHeap();
