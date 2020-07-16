28daf3af8a2fcc3aa572920d7ee3441b6244d216
Molly Seeley <mseeley89@gmail.com>
2020-07-16 18:40:17 -0400

    another basic hash function test

--- 10_Hash_Tables/HashTable.js
+++ 10_Hash_Tables/HashTable.js
@@ -1,6 +1,14 @@
-const hashFunction = (val, length) => {
-  let total = 0;
+//The charCodeAt() method returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index.
 
+const hashFunction = (key, length) => {
+  // let total = 0;
+  // const prime = 31; //prime numbers decrease collisions
+  // for (let i = 0; i < Math.min(key.length, 100); i++) {
+  //   const char = key[i];
+  //   const value = char.charCodeAt(0) - 96;
+  //   total += (total * prime + value) % length;
+  // }
+  // return total;
 }
 
 class HashTable {
--- test/test_Hash_Table.js
+++ test/test_Hash_Table.js
@@ -10,4 +10,10 @@ describe.only("hash function", () => {
     expect(output).to.not.equal(undefined);
     //other possible tests would be for speed and collisions
   });
+  it("does not create duplicate outputs with different inputs", () => {
+    const key1 = "key";
+    const key2 = "key two";
+    const arrayLength = 10;
+    expect(hashFunction(key1, arrayLength)).to.not.equal(hashFunction(key2, arrayLength))
+  })
 });
