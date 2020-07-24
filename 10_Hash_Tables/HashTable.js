52f359561daf8d2b47cc215da5391549eb2533f0
Molly Seeley <mseeley89@gmail.com>
2020-07-24 17:07:57 -0400

    fix hash test and method

--- 10_Hash_Tables/HashTable.js
+++ 10_Hash_Tables/HashTable.js
@@ -1,19 +1,25 @@
 //The charCodeAt() method returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index.
 
 class HashTable {
-  constructor() {
-    this.keyMap = [];
+  constructor(size) {
+    this.keyMap = new Array(size);
   }
+
   hash(key) {
     let total = 0;
     const prime = 31; //prime numbers decrease collisions
     for (let i = 0; i < Math.min(key.length, 100); i++) {
-      const char = key[i];
-      const value = char.charCodeAt(0) - 96;
-      total += (total * prime + value) % this.keyMap.length;
+      const value = key[i].charCodeAt(0) - 96;
+      total = (total * prime + value) % this.keyMap.length;
     }
     return total;
   }
+
+  set(key) {
+    const location = this.hash(key);
+    console.log('location', location)
+  }
+
 }
 
 module.exports = { HashTable };
--- test/test_Hash_Table.js
+++ test/test_Hash_Table.js
@@ -2,8 +2,15 @@ const { expect } = require('chai');
 const { HashTable } = require('../10_Hash_Tables/HashTable');
 
 describe.only("hash function", () => {
+  it("returns a number that is less than the length of the overall HashTable", () => {
+    const hT = new HashTable(10);
+    const key = 'pink';
+    const hashedValue = hT.hash(key);
+    expect(typeof hashedValue).to.equal('number');
+    expect(hashedValue).to.be.lessThan(hT.keyMap.length);
+  });
   it("returns consistent outputs for inputs", () => {
-    const hashTable = new HashTable();
+    const hashTable = new HashTable(10);
     const key = "key";
     const arrayLength = 10
     const output = hashTable.hash(key, arrayLength);
@@ -12,10 +19,19 @@ describe.only("hash function", () => {
     //other possible tests would be for speed and collisions
   });
   it("does not create duplicate outputs with different inputs", () => {
-    const hashTable = new HashTable();
+    const hashTable = new HashTable(10);
     const key1 = "key";
     const key2 = "key two";
     const arrayLength = 10;
     expect(hashTable.hash(key1, arrayLength)).to.not.equal(hashTable.hash(key2, arrayLength))
   })
+});
+describe("set method", () => {
+  it("sets the key based on the hashed value", () => {
+    const hT = new HashTable(10);
+    hT.set("blue");
+    console.log("HT => ", hT);
+  });
+  it("accommodates collisions with separate chaining", () => {
+  });
 });
