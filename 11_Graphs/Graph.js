331408f35cc16a69cc8c21f620cd0fc8a9a4989d
Molly Seeley <mseeley89@gmail.com>
2020-07-31 16:43:33 -0400

    insertEdge

--- 11_Graphs/Graph.js
+++ 11_Graphs/Graph.js
@@ -2,10 +2,20 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
   insertVertex(vertex) {
+    //could add in logic to handle duplicates as opposed to over-write them
     this.adjacencyList[vertex] = [];
-    return this;
   };
+
+  insertEdge(v1, v2) {
+    console.log(this.adjacencyList)
+    if (this.adjacencyList[v1] && this.adjacencyList[v2]) {
+      if (!this.adjacencyList[v1].includes(v2)) this.adjacencyList[v1].push(v2);
+      if (!this.adjacencyList[v2].includes(v1)) this.adjacencyList[v2].push(v1);
+    }
+  }
+
 }
 
 
--- test/test_Graphs.js
+++ test/test_Graphs.js
@@ -4,12 +4,20 @@ const Graph = require('../11_Graphs/Graph');
 
 describe.only("Undirected Graph", () => {
   it("Inserts a Vertex", () => {
+    //could add a test for dealing with duplicate insertions
     const graph = new Graph();
     expect(graph.adjacencyList).to.eql({});
     graph.insertVertex('A');
     expect(graph.adjacencyList).to.eql({ 'A': [] });
   });
-  it("Inserts an Edge", () => { });
-  it("Removes a Vertex", () => { });
-  it("Removes an Edge", () => { });
+  it("Inserts an Edge", () => {
+    //could add a test for dealing with duplicate insertions
+    const graph = new Graph();
+    graph.insertVertex('A');
+    graph.insertVertex('B');
+    graph.insertEdge('A', 'B');
+    expect(graph.adjacencyList).to.eql({ A: ['B'], B: ['A'] });
+  });
+  xit("Removes a Vertex", () => { });
+  xit("Removes an Edge", () => { });
 });
